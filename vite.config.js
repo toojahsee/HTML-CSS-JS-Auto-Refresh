765d25d7c4de9f4270dd5791de29c248774e07b7
toojahsee <toojahsee@gmail.com>
2025-06-14 21:54:27 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
-// 可省略：Vite 默认支持静态资源加载 .glb
-export default {
-  assetsInclude: ['**/*.glb']
-}
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // Render 部署关键路径设置
+  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.bin'], // 可加载 GLTF / BIN 等模型资源
+});
